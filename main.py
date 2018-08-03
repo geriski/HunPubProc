bd26a6fa3d10c3d2891c37e79b4334a19431de67
Iski Gergely <gergely.iski@gmail.com>
2018-08-03 13:35:01 +0200

    Ajánlatkérő added

--- main.py
+++ main.py
@@ -14,13 +14,9 @@ notice_table_names = tree.xpath('//table[@class="notice__heading"]/tr/th/text()'
 for notice_table_name in notice_table_names:
     
     length_name_find = notice_page.find(notice_table_name)
-    #print(length_name_find)
     length_name_start= notice_page[length_name_find:].find('<td>')
-    #print(length_name_start)
     length_name_end = notice_page[length_name_find:].find('</td></tr>')
-    #print(length_name_end)
     tree_name_string = notice_page[length_name_find+length_name_start+4:length_name_find+length_name_end]
-    #print(tree_name_string)
     notice_attributes[notice_table_name] =  tree_name_string
     
     #HREF correction
@@ -29,22 +25,34 @@ for notice_table_name in notice_table_names:
          
         length_name_find = notice_page.find(notice_table_name)
         length_name_start= notice_page[length_name_find:].find('href="')
-        #print(length_name_start)
         length_name_end = notice_page[length_name_find+7:].find('target="')
-        #print(length_name_end)
         tree_name_string = notice_page[length_name_find+length_name_start+7:length_name_find+length_name_end+5]
-        #print(tree_name_string)
         tree_name_string = 'http://www.kozbeszerzes.hu/' + tree_name_string
         notice_attributes[notice_table_name] =  tree_name_string
     
     if notice_table_name == 'Közbeszerzési eljárás:':
         length_name_find = notice_page.find(notice_table_name)
         length_name_start= notice_page[length_name_find:].find('href="')
-        #print(length_name_start)
         length_name_end = notice_page[length_name_find+7:].find('target="')
-        #print(length_name_end)
         tree_name_string = notice_page[length_name_find+length_name_start+6:length_name_find+length_name_end+5]
-        #print(tree_name_string)
         notice_attributes[notice_table_name] =  tree_name_string
+
+#Ajánlatkérő
+notice_attributes['Ajánlakérő:'] ={}
+contracting_authority={}
+length_name_start= notice_page.find('I.1) Név és címek')
+length_name_end = notice_page.find('I.2) Közös közbeszerzés')
+sub_tree_string = notice_page[length_name_start:length_name_end]
+
+parser = etree.HTMLParser()
+sub_tree   = etree.parse(StringIO(sub_tree_string), parser)
+contracting_authority_names = sub_tree.xpath('//div[@style="padding-left:2em;"]/text()')
+for contracting_authority_name in contracting_authority_names:
+    length_name_find = sub_tree_string.find(contracting_authority_name)
+    length_name_start= sub_tree_string[length_name_find:].find('<span')
+    length_name_end = sub_tree_string[length_name_find:].find('</span>')
+    tree_name_string = sub_tree_string[length_name_find+length_name_start+47:length_name_find+length_name_end]
+    contracting_authority[contracting_authority_name] =  tree_name_string
+notice_attributes['Ajánlakérő:'] = contracting_authority
 
 print(notice_attributes)
